4aa097bddbfe083d9fa3de6f6f73e9a07d1896ee
Yannis Markonis <imarkonis@gmail.com>
2018-08-17 11:31:28 +0200

    Making simple code structure

--- bin/get_events.R
+++ bin/get_events.R
@@ -1,29 +1,16 @@
 #Detect and classify the events, i.e., flash droughts
 
-source('./source/libs.R')
+source('./source/libs.R'); source('./source/graphics.R')
 dta <- readRDS('./data/mstat.Rds')
 
-dta_eve <- dta[EVE == T]; rm(dta); gc()
-
-table(dta_eve$yr) # EID seems not to be specific for each grid box; but they are continuous in time
-dta_eve[, ID := .GRP, .(x, y, EID)] # In this way we have event IDs for each grid box
-dta_eve[, dur := .N, ID] # and their duration in months
-dta_eve_CEU <- dta_eve[REG == 'CEU']
-dta_eve_MED <- dta_eve[REG == 'MED']
-
-#For example, the following events happened at grid cell (34, 24) :
-test <- dta_eve[x == 34 & y == 24]
-test[, table(ID)]
-
-#The top 30 droughts in terms of duration in this point are:
-test_top_30_ids <- head(unique(test[order(-dur), .(ID, dur)]), 30)
-test_top_30 <- dta_eve[ID %in% test_top_30_ids$ID]
+dta_CEU <- dta[REG == 'CEU']
+dta_MED <- dta[REG == 'MED']
 
 #The 100 most persisting droughts in all grid cells per region
-top_100_ids_CEU <- head(unique(dta_eve_CEU[order(-dur), .(ID, dur)]), 100)
-top_100_ids_MED <- head(unique(dta_eve_MED[order(-dur), .(ID, dur)]), 100)
-top_100_CEU <- dta_eve[ID %in% top_100_ids_CEU$ID]
-top_100_MED <- dta_eve[ID %in% top_100_ids_MED$ID]
+top_100_ids_CEU <- head(unique(dta_CEU[order(-dur), .(ID, dur)]), 100)
+top_100_ids_MED <- head(unique(dta_MED[order(-dur), .(ID, dur)]), 100)
+top_100_CEU <- dta[ID %in% top_100_ids_CEU$ID]
+top_100_MED <- dta[ID %in% top_100_ids_MED$ID]
 
 #The vegetation period droughts, JJA (strictly 3 months of drought during summer)
 veg_ids_start <- dta_eve[month == 6, ID]
@@ -31,11 +18,11 @@ veg_ids_end <- dta_eve[month == 8, ID]
 veg_dr <- dta_eve[ID %in% veg_ids_start & ID %in% veg_ids_end & dur == 3]
 saveRDS(veg_dr, file = './data/veg_droughts_3_strict.Rds')
 
-#Again strictly 3 months of drought during summer, but with total duration 5 months, i.e. drought continues before or after summer
+#Again strictly 3 months of drought during summer, but with total duration 5 months, i.e., drought continues before or after summer
 veg_dr <- dta_eve[ID %in% veg_ids_start & ID %in% veg_ids_end & dur == 5]
 saveRDS(veg_dr, file = './data/veg_droughts_5_strict.Rds')
 
-#Again strictly 3 months of drought during summer, but not conditional to duration, i.e. all droughts that propagated in summer
+#Again strictly 3 months of drought during summer, but not conditional to duration, i.e., all droughts that propagated in summer
 veg_dr <- dta_eve[ID %in% veg_ids_start & ID %in% veg_ids_end]
 saveRDS(veg_dr, file = './data/veg_droughts_all_strict.Rds')
 
--- bin/init.R
+++ bin/init.R
@@ -9,4 +9,18 @@ dta[, month := month(DTM)]
 dta[, abs_month := .GRP, DTM]
 dta[, PT_ID := .GRP, .(x, y)] #point ids
 
+table(dta_eve[EVE == T]$yr) # EID seems not to be specific for each grid box; but they are continuous in time
+dta[, ID := .GRP, .(x, y, EID)] # In this way we have event IDs for each grid box
+dta[, dur := .N, ID] # and their duration in months
+
 saveRDS(dta, './data/mstat.Rds')
+
+#For example, the following events happened at grid cell (34, 24) :
+test <- dta[x == 34 & y == 24]
+test[, table(ID)]
+
+#The top 30 droughts in terms of duration at this grid cell are:
+test_top_30_ids <- head(unique(test[order(-dur), .(ID, dur)]), 30)
+test_top_30 <- dta_eve[ID %in% test_top_30_ids$ID]
+
+
