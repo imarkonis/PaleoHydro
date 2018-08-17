cd44703789c4ab601ed5b3fd25cb444f4ba5648c
Yannis Markonis <imarkonis@gmail.com>
2018-08-17 12:23:00 +0200

    added nPET

--- bin/init.R
+++ bin/init.R
@@ -18,9 +18,9 @@ dta[!is.na(ID), dur := .N, ID] # and their duration in months
 
 saveRDS(dta, './data/mstat_all.Rds') #dataset used in further analysis
 
-dta_nvars <- dta[, .(EVE, REG, yr, month, abs_month, PT_ID, x, y, ID, dur, nP, nP3, nQ, nS, nT)] 
+dta_nvars <- dta[, .(EVE, REG, yr, month, abs_month, PT_ID, x, y, ID, dur, nP, nP3, nQ, nS, nT, nPET)] 
 saveRDS(dta_nvars, './data/mstat_nvars.Rds') #dataset with normalised values [to work with a smaller data table]
-
+rm(dta, dta_nvars); gc()
 
 ## Examples
 
