8e75c103e49718d53932b158463c4268f1493006
Yannis Markonis <imarkonis@gmail.com>
2018-08-17 12:04:57 +0200

    making code structure more efficient

--- bin/init.R
+++ bin/init.R
@@ -1,26 +1,35 @@
 #First script to run
 
-dir.create("./data"); dir.create("./source"); dir.create("./bin"); dir.create("./results"); dir.create("./results/figs")
+#Folder structure
+ dir.create("./data"); dir.create("./results"); dir.create("./results/figs") #results and data folders are ignored due to size (.gitignore)
 
+source('./source/libs.R') 
+ 
 #Data preparation
 dta <- readRDS('./data/mstat_rQ20_rs_len0_rs_1.Rds') #main analysis dataset
 
-dta[, month := month(DTM)]
+dta[, month := factor(month(DTM))]
 dta[, abs_month := .GRP, DTM]
-dta[, PT_ID := .GRP, .(x, y)] #point ids
+dta[, PT_ID := .GRP, .(x, y)] #id for each grid cell
 
-table(dta_eve[EVE == T]$yr) # EID seems not to be specific for each grid box; but they are continuous in time
-dta[, ID := .GRP, .(x, y, EID)] # In this way we have event IDs for each grid box
-dta[, dur := .N, ID] # and their duration in months
+table(dta[EVE == T]$yr) # EID seems not to be specific for each grid box; but they are continuous in time
+dta[EVE == T, ID := .GRP, .(x, y, EID)] # In this way we have event IDs for each grid box
+dta[!is.na(ID), dur := .N, ID] # and their duration in months
 
-saveRDS(dta, './data/mstat.Rds')
+saveRDS(dta, './data/mstat_all.Rds') #dataset used in further analysis
 
-#For example, the following events happened at grid cell (34, 24) :
+dta_nvars <- dta[, .(EVE, yr, month, abs_month, PT_ID, x, y, ID, nP, nP3, nQ, nS, nT)] 
+saveRDS(dta_nvars, './data/mstat_nvars.Rds') #dataset with normalised values [to work with a smaller data table]
+
+
+## Examples
+
+#Events happened at grid cell (34, 24) :
 test <- dta[x == 34 & y == 24]
 test[, table(ID)]
 
 #The top 30 droughts in terms of duration at this grid cell are:
 test_top_30_ids <- head(unique(test[order(-dur), .(ID, dur)]), 30)
-test_top_30 <- dta_eve[ID %in% test_top_30_ids$ID]
+test_top_30 <- dta[ID %in% test_top_30_ids$ID]
 
 
