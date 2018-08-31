e5d331f5ece1e8f36594829d4876a3e5333684e4
Yannis Markonis <imarkonis@gmail.com>
2018-08-31 13:30:35 +0200

    functions for preconditions and cumsum

--- source/functions.R
+++ source/functions.R
@@ -1,4 +1,32 @@
-library(data.table); library(ggplot2)
+library(data.table); library(ggplot2); library(lubridate); library(zoo)
+
+get_anomaly <- function(dataset, var, event_start, ma_window, duration){  #ma_window in years!
+  dataset[, roll_mean := rollmean(eval(parse(text = var)), k = ma_window, na.pad = T, align = 'right')]
+  event_pr_mean <- dataset[DTM == event_start, roll_mean]
+  out <- dataset[as.Date(DTM) > event_start & as.Date(DTM) < event_start  %m+% months(duration), .(DTM, anomaly = eval(parse(text = var)) - event_pr_mean)]  #perhaps consider monthly means as well
+  return(out)
+} # Calculates the difference between a *var* and its average in the previous *ma_window* years for each value of the next d*uration* years
+
+get_anomaly_events <- function(dataset, var, event_start, ma_window, duration){  #ma_window and duration in years!
+  dataset[, roll_mean := rollmean(eval(parse(text = var)), k = ma_window, na.pad = T, align = 'right')]
+  event_pr_mean <- dataset[DTM %in% event_start, roll_mean]
+  out <- list()
+  for(i in 1:length(event_pr_mean)){
+  out[[i]] <- dataset[as.Date(DTM) > event_start[i] & as.Date(DTM) < event_start[i] %m+% months(duration), 
+                 .(DTM, anomaly = eval(parse(text = var)) - event_pr_mean[i])]
+  }
+  names(out) <- year(event_start)
+  out <- data.table(melt(out, id.vars = c('DTM', 'anomaly'),  varnames = 'event'))
+  colnames(out)[3] <- 'event'
+  out[, event := as.numeric(event)]
+  return(out)
+} 
+
+get_preconditions <- function(dataset, var, event_start, ma_window, duration){
+  out <- get_anomaly_events(dataset, var, event_start %m-% months(duration), ma_window, duration)
+  out[, event := event + floor(duration / 12)]
+  return(out)
+}
 
 add_prv_yr <- function(drought, dataset = dta){
   aa <- unique(drought[, .(PT_ID, yr)]) 
