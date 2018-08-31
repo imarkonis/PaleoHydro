a3eb522395bb38e33f6d984509e3b7c9eab9e1c8
Yannis Markonis <imarkonis@gmail.com>
2018-08-31 13:29:03 +0200

    functions for preconditions

--- bin/preconditions.R
+++ bin/preconditions.R
@@ -1,21 +1,53 @@
 #Explore the preconditions of drought events
 
-source('./source/functions.R'); source('./source/graphics.R') 
-dta <- readRDS('./data/mstat_nvars.Rds')
-veg_dr <- readRDS(file = './data/veg_droughts_AMJ_5_8.Rds')
+library(data.table); library(ggplot2); library(lubridate); library(zoo)
 
-months_before <- 6
-veg_dr_2003 <- veg_dr[yr == 2003, .(PT_ID, yr, month, event, time, nP, nP3, nQ, nS, nT)] 
-veg_dr_2003 <- add_prv_yr(veg_dr_2003)
+get_anomaly <- function(dataset, var, event_start, ma_window, duration){  #ma_window in years!
+  dataset[, roll_mean := rollmean(eval(parse(text = var)), k = ma_window, na.pad = T, align = 'right')]
+  event_pr_mean <- dataset[DTM == event_start, roll_mean]
+  out <- dataset[as.Date(DTM) > event_start & as.Date(DTM) < event_start  %m+% months(duration), .(DTM, anomaly = eval(parse(text = var)) - event_pr_mean)]  #perhaps consider monthly means as well
+  return(out)
+} # Calculates the difference between a *var* and its average in the previous *ma_window* years for each value of the next d*uration* years
 
-abs_start <- unique(veg_dr_2003[month == 3 & event == 'cur_yr', time])
-veg_dr_2003_prv <- veg_dr_2003[time < abs_start & time >= (abs_start - months_before),
-                       .(PT_ID, time, nP, nP3, nQ, nS, nT)]
+get_anomaly_events <- function(dataset, var, event_start, ma_window, duration){  #ma_window and duration in years!
+  dataset[, roll_mean := rollmean(eval(parse(text = var)), k = ma_window, na.pad = T, align = 'right')]
+  event_pr_mean <- dataset[DTM %in% event_start, roll_mean]
+  out <- list()
+  for(i in 1:length(event_pr_mean)){
+  out[[i]] <- dataset[as.Date(DTM) > event_start[i] & as.Date(DTM) < event_start[i] %m+% months(duration), 
+                 .(DTM, anomaly = eval(parse(text = var)) - event_pr_mean[i])]
+  }
+  names(out) <- year(event_start)
+  out <- data.table(melt(out, id.vars = c('DTM', 'anomaly'),  varnames = 'event'))
+  colnames(out)[3] <- 'event'
+  out[, event := as.numeric(event)]
+  return(out)
+} 
 
-to_plot <- melt(data = veg_dr_2003_prv, id.vars = c('PT_ID', 'time'))
-to_plot[, time := time - abs_start]
-plot_var_dens_pre(to_plot)
+get_preconditions <- function(dataset, var, event_start, ma_window, duration){
+  out <- get_anomaly_events(dataset, var, event_start %m-% months(duration), ma_window, duration)
+  out[, event := event + floor(duration / 12)]
+  return(out)
+}
 
+add_prv_yr <- function(drought, dataset = dta){
+  aa <- unique(drought[, .(PT_ID, yr)]) 
+  aa_prv <- aa
+  aa$event = factor('cur_yr')
+  aa_prv$event = factor('prv_yr')
+  aa_prv[, yr := yr - 1] 
+  aa <- rbind(aa, aa_prv)
+  out <- merge(aa, dataset)
+  return(out)
+}
 
-
-
+add_nxt_yr <- function(drought, dataset = dta){
+  aa <- unique(drought[, .(PT_ID, yr)]) 
+  aa_nxt <- aa
+  aa$event = factor('cur_yr')
+  aa_nxt$event = factor('nxt_yr')
+  aa_nxt[, yr := yr + 1] 
+  aa <- rbind(aa, aa_nxt)
+  out <- merge(aa, dataset)
+  return(out)
+}
