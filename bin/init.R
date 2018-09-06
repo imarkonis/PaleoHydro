54c1694a5547cf6a109e986df9c2084f56b773b2
Yannis Markonis <imarkonis@gmail.com>
2018-09-06 12:22:46 +0200

    new dataset

--- bin/init.R
+++ bin/init.R
@@ -6,26 +6,26 @@
 source('./source/functions.R') 
  
 #Data preparation
-dta <- readRDS('./data/mstat_rQ20_rs_len0_rs_1.Rds') #main analysis dataset
+dta <- readRDS('./data/raw/mstat_rQ20_rs_len0_met_001.Rds') #main analysis dataset
 
 dta[, yr := year(DTM)]
 dta[, month := month(DTM)]
-dta[, time := .GRP, DTM] #absolute time units
+#dta[, time := .GRP, DTM] #absolute time units
 dta[, PT_ID := .GRP, .(x, y)] #id for each grid cell
 
-table(dta[EVE == T]$yr) #EID seems not to be specific for each grid box; but they are continuous in time
+table(dta[EVE == T]$EID) #EID is not unique for each event - serial number of event at each grid cell (below q20)
 dta[EVE == T, ID := .GRP, .(x, y, EID)] #in this way we have event IDs for each grid box
 dta[EVE == T, start := min(DTM, na.rm = T), ID] 
-dta[EVE == T, start_abs := min(time, na.rm = T), ID] 
+#dta[EVE == T, start_abs := min(time, na.rm = T), ID] 
 dta[EVE == T, start_month := month(min(DTM, na.rm = T)), ID] 
 dta[!is.na(ID), dur := .N, ID] #and their duration in months 
 setorder(dta, PT_ID)
 
-saveRDS(dta, './data/mstat_all.Rds') #dataset used in further analysis
+saveRDS(dta, './data/mstat_all_met1.Rds') #dataset used in further analysis
 
-dta_nvars <- dta[, .(EVE, REG, DTM, yr, month, time, PT_ID, x, y, ID, start, start_month, dur, nP, nP3, nQ, nS, nT, nPET)] 
-saveRDS(dta_nvars, './data/mstat_nvars.Rds') #dataset with normalised values [to work with a smaller data table]
-rm(dta, dta_nvars)
+#dta_nvars <- dta[, .(EVE, REG, DTM, yr, month, time, PT_ID, x, y, ID, start, start_month, dur, nP, nP3, nQ, nS, nT, nPET)] 
+#saveRDS(dta_nvars, './data/mstat_nvars_met1.Rds') #dataset with normalised values [to work with a smaller data table]
+#rm(dta, dta_nvars)
 
 ## Examples
 
