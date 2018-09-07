e2d202d425b44f60cf86be1ca9e09541a1c6a6d8
Yannis Markonis <imarkonis@gmail.com>
2018-09-07 14:28:01 +0200

    coordinate tranformations

--- bin/space_aid.R
+++ bin/space_aid.R
@@ -12,5 +12,3 @@ saveRDS(dta_sp, file = './data/spatial.Rds')
 dta_sp_rnd <- dta_sp[runif(100, 1, max(dta_sp$PT_ID)), ]
 plot(dta_sp_rnd$x, dta_sp_rnd$y, col = 'white') 
 text(dta_sp_rnd$x, y = dta_sp_rnd$y, labels = dta_sp_rnd$PT_ID)
-
-#lat, lon in rs_met_001.rds
