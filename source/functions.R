23058f8f82ffd0e3696f7311268f5d64173df380
Yannis Markonis <imarkonis@gmail.com>
2019-02-21 09:41:56 +0100

    new functions

--- source/functions.R
+++ source/functions.R
@@ -1,7 +1,6 @@
 library(data.table); library(ggplot2); library(lubridate); library(zoo); library(scales) 
 library(ncdf4); library(doSNOW); library(reshape2)
-
-get.periods <- function(dataset, eve, pre, aft){
+get_periods <- function(dataset, eve, pre, aft){
   events <- unique(eve[, .(PT_ID, start, dur, event = year(start))])
   events[, start_pre := start - months(pre)]
   events[, start_aft := start + months(dur) + months(aft)]
@@ -14,7 +13,7 @@ get.periods <- function(dataset, eve, pre, aft){
   return(out)
 }
 
-get.periods.par <- function(dataset, eve, pre, aft){
+get_periods_par <- function(dataset, eve, pre, aft){
   no_cores <- as.numeric(Sys.getenv('NUMBER_OF_PROCESSORS')) - 1
   if(no_cores < 1 | is.na(no_cores))(no_cores <- 1)
   cluster = makeCluster(no_cores, type = "SOCK")
@@ -33,16 +32,28 @@ get.periods.par <- function(dataset, eve, pre, aft){
   return(out)
 }
 
-cumsum.events <-  function(dataset, eve, pre, aft, scale = T, par = F){
+local_scale <- function(dataset, event, no_yrs = 30){
+  dr_start <- unique(min(event$DTM))
+  if(dr_start - unique(dataset[, min(start, na.rm = T)]) - no_yrs < 0) {
+    out <- dataset[DTM > dr_start & DTM <= dr_start + years(no_yrs)]
+    } else {
+    out <- dataset[DTM < dr_start & DTM >= dr_start - years(no_yrs)]
+    }
+  out <- merge(out, event, all = T)
+  out[, paste0('value_anom_', no_yrs) := (value - mean(value, na.rm = T)), variable]
+  out[, paste0('value_z_', no_yrs) := scale(value), variable]
+  return(out[DTM >= dr_start])
+}
+
+cumsum_events <-  function(dataset, eve, pre, aft, scale = 'value', par = F){
   if(par == F) (oo <- get_periods(dataset, eve, pre, aft))
   else(oo <- get_periods_par(dataset, eve, pre, aft))
   out = dataset[oo, on = c('DTM', 'PT_ID')]
-  if(scale == T) (out[, cumsum := cumsum(anom_z), c('event', 'variable', 'PT_ID')])
-  else (out[, cumsum := cumsum(anom), c('event', 'variable', 'PT_ID')])
+  out[, cumsum := cumsum(eval(parse(text = scale))), c('event', 'variable', 'PT_ID')]
   return(out)
 }
 
-space.prpg <- function(event, thres = 0.1, same_year = F){ #Spatial propagation of drought event derived from maximum number of points
+space_prpg <- function(event, thres = 0.1, same_year = F){ #Spatial propagation of drought event derived from maximum number of points
   peak <- as.Date(apply(table(event[!is.na(value) & !is.na(start), DTM, variable]), 1, function(x) names(which.max(x))))
   peak_pts <- unique(event[DTM %in% peak, PT_ID])
   start <- event[!is.na(value) & !is.na(start), .N, .(variable, start)]
